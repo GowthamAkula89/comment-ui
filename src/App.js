c2a3c399f1490072d3cc483b8eb7dbd67fcd7f8b
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-08-26 13:25:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/header';
 import { AuthProvider } from './Contexts/AuthContext/authContext';
+import Main from './Components/Main/main';
+import { SnackbarProvider } from 'notistack';
+import { Provider } from 'react-redux';
+import store from './Redux/store';
+
 function App() {
   return (
     <div className="App">
-       <AuthProvider>
-       <Header/>
-       </AuthProvider>
-  
+      <SnackbarProvider  maxSnack={2} autoHideDuration={3000}>
+        <AuthProvider>
+          <Provider store={store}>
+            <Header/>
+            <Main/>
+          </Provider>
+        </AuthProvider>
+      </SnackbarProvider>
     </div>
   );
 }
