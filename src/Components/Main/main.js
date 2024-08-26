c126e1556d06b5b5b8d4271715e237fadcddb64f
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-08-26 22:27:07 +0530

    Update Filters

--- src/Components/Main/main.js
+++ src/Components/Main/main.js
@@ -4,15 +4,17 @@ import { getDatabase, ref, get } from "firebase/database";
 import { useAuth } from "../../Contexts/AuthContext/authContext";
 import Reaction from "../Reactions/reactions";
 import "./main.css";
-import { setComments } from "../../Redux/Slices/comments.slice";
+import { setComments, setActiveSort } from "../../Redux/Slices/comments.slice";
 import { useDispatch, useSelector } from "react-redux";
 import addComments from "../../APIs/addComments";
 import { useSnackbar } from "notistack";
 import RepliesPagination from "../RepliesPagination/repliesPagination";
+import { sortComments } from "../../Utils/sortComments";
 
 const Main = () => {
   const { currentUser } = useAuth();
   const comments = useSelector((state) => state.comments.comments);
+  const activeSort = useSelector((state) => state.comments.activeSort);
   const [newComment, setNewComment] = useState("");
   const dispatch = useDispatch();
   const { enqueueSnackbar } = useSnackbar();
@@ -27,7 +29,8 @@ const Main = () => {
       const dbRef = ref(db, "comments");
       const data = await get(dbRef);
       if (data.exists()) {
-        dispatch(setComments(data.val()));
+        const sortedComments = sortComments(data.val(), activeSort);
+        dispatch(setComments(sortedComments));
       }
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -51,38 +54,65 @@ const Main = () => {
     
   };
 
+  
+  const handleFilter = (sortOption) => {
+    const sortedComments = sortComments(comments, sortOption);
+    dispatch(setComments(sortedComments));
+    dispatch(setActiveSort(sortOption));
+  }
+  
+  console.log("FIltered", comments)
   return (
     <div className="main">
-      <div className="comment-input-section">
-        {currentUser && (
-          <>
-            <input
-              type="text"
-              value={newComment}
-              onChange={(e) => setNewComment(e.target.value)}
-              placeholder="Write a comment..."
-              className="comment-input"
-            />
-            <button onClick={saveData} className="comment-submit-btn">
-              Submit
-            </button>
-          </>
-        )}
+      <div className="sort-options">
+        <button 
+          onClick={() => handleFilter("latest")} 
+          className={activeSort === "latest" ? "active" : ""}
+        >
+          Latest
+        </button>
+        <button 
+          onClick={() => handleFilter("popular")} 
+          className={activeSort === "popular" ? "active" : ""}
+        >
+          Popular
+        </button>
       </div>
-      {Object.entries(comments).map(([key, comment]) => (
-        <div className="comment-container" key={key}>
-          <div className="user-data">
-            <img src={comment.userProfile} alt="user-photo" className="user-img" />
-            <div className="user-name">{comment.userName}</div>
-          </div>
-          <CommentText text={comment.comment} />
-          <Reaction commentKey={key} handleFetch={handleFetch} comment={comment} />
-          {comment.replies && (
-            <RepliesPagination replies={comment.replies} commentKey={key} handleFetch={handleFetch} CommentText={CommentText}/>
-          )}
-        </div>
-      ))}
+    <div className="comment-input-section">
+      {currentUser && (
+        <>
+          <input
+            type="text"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            placeholder="Write a comment..."
+            className="comment-input"
+          />
+          <button onClick={saveData} className="comment-submit-btn">
+            Submit
+          </button>
+        </>
+      )}
     </div>
+    {Object.entries(comments).map(([key, comment]) => (
+      <div className="comment-container" key={key}>
+        <div className="user-data">
+          <img src={comment.userProfile} alt="user-photo" className="user-img" />
+          <div className="user-name">{comment.userName}</div>
+        </div>
+        <CommentText text={comment.comment} />
+        <Reaction commentKey={key} handleFetch={handleFetch} comment={comment} />
+        {comment.replies && (
+          <RepliesPagination replies={comment.replies} commentKey={key} handleFetch={handleFetch} CommentText={CommentText}/>
+        )}
+        {/* {comment.replies &&
+          Object.entries(comment.replies).map(([key2, reply2]) => (
+            <RepliesPagination replies={reply2.replies} commentKey={key2} handleFetch={handleFetch} CommentText={CommentText}/>
+          ))
+        } */}
+      </div>
+    ))}
+  </div>
   );
 };
 
