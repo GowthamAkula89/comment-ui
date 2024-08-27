eae934e3d4aea4cf4618db2b6bcff69a35bcfb56
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-08-27 08:46:31 +0530

    Update firebase.js

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -2,17 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration using environment variables
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDdpJv68dX9ZsaAu8qSsRLVzteLMB1hsWs",
+  authDomain: "alter-f3ed6.firebaseapp.com",
+  databaseURL: "https://alter-f3ed6-default-rtdb.firebaseio.com",
+  projectId: "alter-f3ed6",
+  storageBucket: "alter-f3ed6.appspot.com",
+  messagingSenderId: "745212942732",
+  appId: "1:745212942732:web:abfdbc7a13861e7fbd5ea9",
+  measurementId: "G-99CD8ECVHR"
 };
 
 // Initialize Firebase
